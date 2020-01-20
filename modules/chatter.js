38e9e0774ae80c77bb88ba691e6c7bf8119555c2
Chirag Paliwal <chirag.paliwal06@gmail.com>
2020-01-20 18:22:25 +0530

    fix1.1

--- modules/chatter.js
+++ modules/chatter.js
@@ -32,7 +32,7 @@ exports.execute = (req, res) => {
             if (error.code == 401) {
                 res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
             } else {
-                res.send( error + "An error as occurred");
+                res.send( JSON.stringify(error) + "An error as occurred");
             }
         });
     /*
